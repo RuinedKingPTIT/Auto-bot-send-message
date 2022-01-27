41b431d6b259b3e457c81f792d1150977b814575
NguyenVanSang-coder <62025815+NguyenVanSang-coder@users.noreply.github.com>
2022-01-27 08:35:17 +0700

    first commit

---
+++ src/main/java/com/crush/botMessage/Auto/bot/send/message/AutoBotSendMessageApplication.java
@@ -0,0 +1,13 @@
+package com.crush.botMessage.Auto.bot.send.message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoBotSendMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoBotSendMessageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crush/botMessage/Auto/bot/send/message/AutoBotSendMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crush.botMessage.Auto.bot.send.message;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoBotSendMessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
